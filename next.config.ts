61728583cbc31166d6bdcc7ea87f8965dd44e5de
Ameer Hamza <hamzaataariq12@gmail.com>
2025-03-17 23:37:28 +0500

    debug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+  typescript:{
+    ignoreBuildErrors:true
+  }
 };
 
 export default nextConfig;
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
